274d4d2d3edc93a22c93636d6d5c9da882571e1a
silas <jose.silasjr.bb@gmail.com>
2016-10-21 11:15:32 -0400

    inserido atributo

--- src/main/java/br/com/caelum/blog/jenkins/Orcamento.java
+++ src/main/java/br/com/caelum/blog/jenkins/Orcamento.java
@@ -2,6 +2,7 @@ package br.com.caelum.blog.jenkins;
 public class Orcamento {
 
 	private double valor;
+        private double orcamentoFavoravel;
 
 	public Orcamento(double valor) {
 		this.valor = valor;
@@ -9,6 +10,14 @@ public class Orcamento {
 
 	public double getValor() {
 		return valor;
+	}
+
+	public double getOrcamentoFavoravel() {
+		return this.orcamentoFavoravel;
+	}
+
+	public void setOrcamentoFavoravel(double orcamentoFavoravel) {
+		this.orcamentoFavoravel = orcamentoFavoravel;
 	}
 
 }
